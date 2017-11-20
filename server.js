773ff451ac24e7c5ab5e7333a33282a793b2696b
“Sophia” <jungwon.lim.0210@gmail.com>
2017-11-20 11:55:51 +0200

    server uploads files from public folder

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var fs = require('fs');
+// var mime = require('mime');
 
 var message = "We are so happy to be part of the Node Girls workshop";
 
-function handler(request, response){
+const extensionType = {
+  'html' : 'text/html',
+  'css' : 'text/css',
+  'js' : 'application/javascript',
+  'ico' : 'image/x-icon',
+  'png' : 'png',
+  'jpg' : 'jpeg/Sophia'
+};
+
+function handler(request, response) {
   var endpoint = request.url;
   console.log(endpoint);
   var method = request.method;
@@ -11,35 +21,55 @@ function handler(request, response){
   console.log('type of request', typeof(request));
 
   if (endpoint === "/") {
-    response.writeHead(200, {"Content-Type":"text/html"});
+
     fs.readFile(__dirname + '/public/index.html',
-    function(error, file) {
-      if (error) {
+      function(error, file) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        response.writeHead(200, {
+          "Content-Type": "text/html"
+        });
+        response.end(file);
+      });
+
+  } else if (endpoint === '/node') {
+    response.write('node url');
+    response.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    response.end();
+
+  } else if (endpoint === '/girls') {
+    response.write('girls url');
+    response.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    response.end();
+
+  } else {
+    fs.readFile(__dirname + '/public' + request.url,
+    function(error, file){
+      if (error){
         console.log(error);
         return;
       }
-
+      //console.log('file content', mime.getType(request.url))
+      var extension = request.url.split('.')[1];
+      console.log('extension',extension);
+      console.log('extension type', extensionType[extension]);
+      response.writeHead(200, {
+        "Content-type": extensionType[extension]
+      });
       response.end(file);
     });
-  }
 
-  else if (endpoint === '/node') {
-    response.writeHead(200, {"Content-type":"text/html"});
-    response.write('node url');
-    response.end();
-  } else if (endpoint === '/girls') {
-    response.writeHead(200, {"Content-type":"text/html"});
-    response.write('girls url');
-    response.end();
-  } else {
-    response.writeHead(200, {"Content-type":"text/html"});
-    response.write(message);
-    response.end();
   }
 }
 
 var server = http.createServer(handler);
 
 server.listen(3000, function() {
-console.log("Server is listening on port 3000. Ready to accept requests!");
+  console.log("Server is listening on port 3000. Ready to accept requests!");
 });
