c2a9e02d50fccd0a36347a950f2cbc9f06106101
“Sophia” <jungwon.lim.0210@gmail.com>
2017-11-20 12:43:21 +0200

    created form and made it load on the same page

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring');
 // var mime = require('mime');
 
 var message = "We are so happy to be part of the Node Girls workshop";
+var allTheData = '';
+
 
 const extensionType = {
-  'html' : 'text/html',
-  'css' : 'text/css',
-  'js' : 'application/javascript',
-  'ico' : 'image/x-icon',
-  'png' : 'png',
-  'jpg' : 'jpeg/Sophia'
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'application/javascript',
+  'ico': 'image/x-icon',
+  'png': 'png',
+  'jpg': 'jpeg/Sophia'
 };
 
 function handler(request, response) {
@@ -48,25 +51,49 @@ function handler(request, response) {
     });
     response.end();
 
+  } else if (endpoint === '/create-post') {
+
+    request.on('data', function(chunkOfData) {
+      allTheData += chunkOfData;
+    });
+
+    request.on('end', function() {
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.writeHead(302, {
+        "Location": "/"
+      });
+      response.end();
+    });
+
+
+
+
   } else {
     fs.readFile(__dirname + '/public' + request.url,
-    function(error, file){
-      if (error){
-        console.log(error);
-        return;
-      }
-      //console.log('file content', mime.getType(request.url))
-      var extension = request.url.split('.')[1];
-      console.log('extension',extension);
-      console.log('extension type', extensionType[extension]);
-      response.writeHead(200, {
-        "Content-type": extensionType[extension]
+      function(error, file) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        //console.log('file content', mime.getType(request.url))
+        var extension = request.url.split('.')[1];
+        console.log('extension', extension);
+        console.log('extension type', extensionType[extension]);
+        response.writeHead(200, {
+          "Content-type": extensionType[extension]
+        });
+        response.end(file);
       });
-      response.end(file);
-    });
 
   }
 }
+
+
+
+
+
+
 
 var server = http.createServer(handler);
 
